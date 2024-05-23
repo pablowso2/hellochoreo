3d6bc447105d7cf2c52d92b78db11f86646b9b6b
PabloSA <pablosa@wso2.com>
2024-05-23 16:11:29 +0200

    hello

--- hello/app.py
+++ hello/app.py
@@ -1,12 +1,10 @@
-import json
-import ast
-from flask import Flask, request
-from types import SimpleNamespace
+from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/hello', methods=['GET'])
 def hello():
-    return "Hello, Welcome to the simple reservation management app!"
+    return "Hello, Welcome Choreo!"
 
 if __name__ == "__main__":
     app.run()
